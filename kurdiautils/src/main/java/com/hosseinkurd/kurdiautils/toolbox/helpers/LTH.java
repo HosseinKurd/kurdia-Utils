115f32a517a88ee8c6a7545d5964ebcdc6e8b5dc
Kurdia <ho.kurd@gmail.com>
2018-11-05 16:55:10 +0330

    1397-08-14 I

--- kurdiautils/src/main/java/com/hosseinkurd/kurdiautils/toolbox/helpers/LTH.java
+++ kurdiautils/src/main/java/com/hosseinkurd/kurdiautils/toolbox/helpers/LTH.java
@@ -2,25 +2,31 @@ package com.hosseinkurd.kurdiautils.toolbox.helpers;
 
 import android.util.Log;
 
+import com.hosseinkurd.kurdiautils.BuildConfig;
+
 /**
  * Created by Kurdia on 4/15/17.
  */
 
 public class LTH {
+    private LTH() {
+
+    }
 
     public static void iLog(String tag, String msg) {
         iLog(tag, msg, null);
     }
 
     public static void iLog(String tag, String msg, Throwable throwable) {
-        int maxLogSize = 1000;
-        for(int i = 0; i <= msg.length() / maxLogSize; i++) {
-            int start = i * maxLogSize;
-            int end = (i+1) * maxLogSize;
-            end = end > msg.length() ? msg.length() : end;
-            Log.i(tag, msg.substring(start, end), throwable);
+        if (BuildConfig.DEBUG) {
+            int maxLogSize = 1000;
+            for (int i = 0; i <= msg.length() / maxLogSize; i++) {
+                int start = i * maxLogSize;
+                int end = (i + 1) * maxLogSize;
+                end = end > msg.length() ? msg.length() : end;
+                Log.i(tag, msg.substring(start, end), throwable);
+            }
         }
-
     }
 
     public static void dLog(String tag, String msg) {
@@ -28,12 +34,14 @@ public class LTH {
     }
 
     public static void dLog(String tag, String msg, Throwable throwable) {
-        int maxLogSize = 1000;
-        for(int i = 0; i <= msg.length() / maxLogSize; i++) {
-            int start = i * maxLogSize;
-            int end = (i+1) * maxLogSize;
-            end = end > msg.length() ? msg.length() : end;
-            Log.d(tag, msg.substring(start, end), throwable);
+        if (BuildConfig.DEBUG) {
+            int maxLogSize = 1000;
+            for (int i = 0; i <= msg.length() / maxLogSize; i++) {
+                int start = i * maxLogSize;
+                int end = (i + 1) * maxLogSize;
+                end = end > msg.length() ? msg.length() : end;
+                Log.d(tag, msg.substring(start, end), throwable);
+            }
         }
     }
 
@@ -42,12 +50,14 @@ public class LTH {
     }
 
     public static void eLog(String tag, String msg, Throwable throwable) {
-        int maxLogSize = 1000;
-        for(int i = 0; i <= msg.length() / maxLogSize; i++) {
-            int start = i * maxLogSize;
-            int end = (i+1) * maxLogSize;
-            end = end > msg.length() ? msg.length() : end;
-            Log.e(tag, msg.substring(start, end), throwable);
+        if (BuildConfig.DEBUG) {
+            int maxLogSize = 1000;
+            for (int i = 0; i <= msg.length() / maxLogSize; i++) {
+                int start = i * maxLogSize;
+                int end = (i + 1) * maxLogSize;
+                end = end > msg.length() ? msg.length() : end;
+                Log.e(tag, msg.substring(start, end), throwable);
+            }
         }
     }
 
